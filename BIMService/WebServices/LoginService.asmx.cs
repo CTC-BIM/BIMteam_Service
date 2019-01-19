c9cb5f09cd512422f2b36b42b09abc2343aef53f
nhantc <nhantc@coteccons.vn>
2019-01-19 08:27:57 +0700

    Add login method

---
+++ BIMService/WebServices/LoginService.asmx.cs
@@ -0,0 +1,32 @@
+﻿using BIMService.WebServices.Members;
+using System.Web.Services;
+
+namespace BIMService.WebServices
+{
+    /// <summary>
+    /// Summary description for LoginService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class LoginService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public MemberOutput Login(string userName, string password)
+        {
+            try
+            {
+                if (userName == null || userName.Trim() == "" || password == null || password.Trim() == "") return null;
+                //Viết thêm hàm login
+                return null;
+            }
+            catch (System.Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
